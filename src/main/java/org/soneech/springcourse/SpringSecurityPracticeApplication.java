c9c33db61b265b70297eb432561052d1be34d1a5
Soneech <sonya.patrina@yandex.ru>
2023-09-13 19:55:32 +0300

    Initial commit

---
+++ src/main/java/org/soneech/springcourse/SpringSecurityPracticeApplication.java
@@ -0,0 +1,12 @@
+package org.soneech.springcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPracticeApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityPracticeApplication.class, args);
+	}
+
+}
