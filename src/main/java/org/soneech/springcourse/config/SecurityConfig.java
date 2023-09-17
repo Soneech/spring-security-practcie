5a3e0c10fef76c4c43bb34885423dab219ab0a9a
Soneech <sonya.patrina@yandex.ru>
2023-09-17 16:10:26 +0300

    Custom login page and authorization

--- src/main/java/org/soneech/springcourse/config/SecurityConfig.java
+++ src/main/java/org/soneech/springcourse/config/SecurityConfig.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
@@ -17,6 +19,26 @@ public class SecurityConfig {
     @Autowired
     public SecurityConfig(UserDetailsServiceImpl userDetailsService) {
         this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                    .requestMatchers("/auth/login", "/error").permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                    .formLogin().loginPage("/auth/login")
+                    .loginProcessingUrl("/process_login")
+                    .defaultSuccessUrl("/", true)
+                    .failureUrl("/auth/login?error")
+                .and()
+                    .logout()
+                    .logoutUrl("/logout")
+                    .logoutSuccessUrl("/auth/login")
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID");
+        return http.build();
     }
 
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
