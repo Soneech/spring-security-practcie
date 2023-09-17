b780cd6479b1d8f874e8a7bfb7b0115890365910
Soneech <sonya.patrina@yandex.ru>
2023-09-17 16:31:37 +0300

    With using latest version of spring boot

--- src/main/java/org/soneech/springcourse/config/SecurityConfig.java
+++ src/main/java/org/soneech/springcourse/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.soneech.springcourse.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -24,20 +25,19 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests()
-                    .requestMatchers("/auth/login", "/error").permitAll()
-                    .anyRequest().authenticated()
-                .and()
-                    .formLogin().loginPage("/auth/login")
-                    .loginProcessingUrl("/process_login")
-                    .defaultSuccessUrl("/", true)
-                    .failureUrl("/auth/login?error")
-                .and()
-                    .logout()
-                    .logoutUrl("/logout")
-                    .logoutSuccessUrl("/auth/login")
-                    .invalidateHttpSession(true)
-                    .deleteCookies("JSESSIONID");
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/auth/login", "/error").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(form -> form
+                        .loginPage("/auth/login")
+                        .loginProcessingUrl("/process_login")
+                        .defaultSuccessUrl("/", true)
+                        .failureUrl("/auth/login?error"))
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/auth/login")
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID"));
         return http.build();
     }
 
